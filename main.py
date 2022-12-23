82cabff82f9dcf95d72fe82b2f97fdae94f2147f
anddp12 <anddp12@gmail.com>
2022-12-23 23:49:27 +0200

    add API, application json, requests

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import json, requests
 from keys import API_KEY
 
+URL ='https://api.openweathermap.org/data/2.5/weather'
+QUERY_CITY = "?q={0}&appid={1}"
+
+city = input('City: ')
+request = URL + QUERY_CITY.format(city, API_KEY)
+response = requests.get(request)
+
+json_w = json.loads(response.text)
+print(json_w)
